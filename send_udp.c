3096d3eee1ad96a7915d87d81a9835f1c8897b12
Luis Franco <luisfranco.business@gmail.com>
2024-10-25 09:48:04 -0600

    Got fd to communicate, Read and send UDP packets.
    Iterated over getaddrinfo linked list to get file descriptor.
    Read at most 480 bytes from stdin at a time.
    Send packets of at most 480 bytes through UDP.
    Send a 0 byte packet through UDP for EOF.

--- send_udp.c
+++ send_udp.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <errno.h>
 #include <netdb.h>
+#include <unistd.h>
 
 #define BUFFER_LEN ((size_t) 480)
 
@@ -11,6 +12,12 @@ int main(int argc, char **argv) {
 	struct addrinfo hints;
 	int gai_code;
 	struct addrinfo *result;
+	struct addrinfo *curr;
+	int found;
+	int fd;
+	ssize_t read_res;
+	size_t bytes_to_be_sent;
+	char buf[480];
 
 	/* Checks arguments for server name and port name */
 	if (argc < 3) {
@@ -29,6 +36,65 @@ int main(int argc, char **argv) {
 	hints.ai_protocol = 0;
 	hints.ai_flags = 0;
 	gai_code = getaddrinfo(server_name, port_name, &hints, &result);
+	if (gai_code != 0) {
+		fprintf(stderr, "Could not look up server address info for %s %s\n",
+			server_name, port_name);
+		return 1;
+	}
 
+	/* Iterate over the linked list returned by getaddrinfo */
+	found = 0;
+	for (curr=result; curr !=NULL; curr=curr->ai_next) {
+		fd = socket(curr->ai_family, curr->ai_socktype, curr->ai_protocol);
+		if (fd >= 0) {
+			if (connect(fd, curr->ai_addr, curr->ai_addrlen) >= 0) {
+				found = 1;
+				break;
+			} else {
+				if (close(fd) < 0) {
+					fprintf(stderr, "Could not close a socket: %s\n",
+						strerror(errno));
+					freeaddrinfo(result);
+					return 1;
+				}
+			}
+		}
+	}
+	freeaddrinfo(result);
+	if (!found) {
+		fprintf(stderr, "Could not connect to any possible results for %s %s \n",
+			server_name, port_name);
+		return 1;
+	}
+
+	/* Now we need to read stdin and send UDP packets is chunks of 
+	at most 480 bytes.
+	*/
+	for (;;) {
+		read_res = read(0, buf, BUFFER_LEN);
+		if (read_res == ((ssize_t) 0)) {
+			/* We have hit EOF send UDP packet of size 0 to peer */
+			bytes_to_be_sent = (size_t) 0;
+			if (send(fd, buf, bytes_to_be_sent, 0) < ((ssize_t) 0)) {
+				fprintf(stderr, "Error with send: %s\n",
+					strerror(errno));
+				return 1;
+			}
+			break;
+		}
+		if (read_res < ((ssize_t) 0)) {
+			/* Error on read() */
+			fprintf(stderr, "Error using read: %s\n",
+				strerror(errno));
+			return 1;
+		}
+		bytes_to_be_sent = (size_t)read_res;
+		if (send(fd, buf, bytes_to_be_sent, 0) < ((ssize_t) 0)) {
+			fprintf(stderr, "Error with send: %s\n",
+				strerror(errno));
+			return 1;
+		}
+	}
+	// TODO Close fd
 	return 0;
 }
