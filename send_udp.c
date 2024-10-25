cce07625b2ebfdf0a353366d79f64b8ac9487ca0
Luis Franco <luisfranco.business@gmail.com>
2024-10-25 08:25:26 -0600

    Added provided hints and defined buffer length
    Buffer length is 480 bytes. Hints for getaddrinfo. Also added more libraries.

--- send_udp.c
+++ send_udp.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <netdb.h>
+
+#define BUFFER_LEN ((size_t) 480)
 
 int main(int argc, char **argv) {
 	char *server_name;
 	char *port_name;
+	struct addrinfo hints;
+	int gai_code;
+	struct addrinfo *result;
+
 	/* Checks arguments for server name and port name */
 	if (argc < 3) {
 		fprintf(stderr, "Not enough arguments.");
@@ -10,6 +19,16 @@ int main(int argc, char **argv) {
 	}
 	server_name = argv[1];
 	port_name = argv[2];
+	
+	/* Use getaddrinfo() to get the right type of address information
+	for the name and port we have got.
+	*/
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_protocol = 0;
+	hints.ai_flags = 0;
+	gai_code = getaddrinfo(server_name, port_name, &hints, &result);
 
 	return 0;
 }
