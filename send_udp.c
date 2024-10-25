a22354052bb73e60fb36c048f999e6a0afddae24
Luis Franco <luisfranco.business@gmail.com>
2024-10-25 08:12:20 -0600

    Added argument checking
    First argument is server name, second argument is port.

--- send_udp.c
+++ send_udp.c
@@ -1,3 +1,15 @@
-int main (int argc, char **argv) {
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	char *server_name;
+	char *port_name;
+	/* Checks arguments for server name and port name */
+	if (argc < 3) {
+		fprintf(stderr, "Not enough arguments.");
+		return 1;
+	}
+	server_name = argv[1];
+	port_name = argv[2];
+
 	return 0;
 }
